991794fed829bbed8e681b9f15b488bf61e6d6bc
Johan Navarro <johannavarro7@gmail.com>
2023-07-28 10:11:02 -0500

    commit 1

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,58 +3,66 @@ import * as S from "./AboutMe.styles";
 import logoGithub from "../../svg/github.svg";
 import logoLinkeind from "../../svg/linkedin.svg";
 import logoYoutube from "../../svg/youtube.svg";
+import foto from "../../images/projects/foto.jpg";
 
 const AboutMe = () => {
-    const socialNetworks =[
-        {
-            name:"Github",
-            link:"https://github.com/ivandevia",
-            logo:logoGithub,
-        },
-        {
-            name:"Linkedind",
-            link:"www.linkedin.com/in/ivan-devia-acosta-38b9aa141",
-            logo:logoLinkeind,
-        },
-        {
-            name:"Youtube",
-            link:"https://www.youtube.com/channel/UC2Gv4AvSJOPFQ6pjGFk7U2g",
-            logo:logoYoutube,
-        },
-    ];
+  const socialNetworks = [
+    {
+      name: "Github",
+      link: "https://github.com/Johan10N/",
+      logo: logoGithub,
+    },
+    {
+      name: "Linkedind",
+      link: "https://www.linkedin.com/in/johan-navarro-rodriguez-853b94285/",
+      logo: logoLinkeind,
+    },
+    {
+       name: "Youtube",
+       link: "https://www.youtube.com/channel/UC2Gv4AvSJOPFQ6pjGFk7U2g",
+       logo: logoYoutube,
+    },
+  ];
 
-    return (
-        <S.ContainerAboutMe id="about-me">
-            <S.ContainerFlex>
-                <S.ContainerText>
-                    <S.Title>
-                        <p>👋🏼 Hello, i'm</p>
-                        <p>Iván Devia Acosta</p>
-                        <p>Backend Developer</p>
-                    </S.Title>
-                    <S.DescriptionText>
-                    Ing. de Sistemas, Mg. en Gestión de Tecnología Educativa, 
-                    Maestrante en Dirección Estratégica en Ing de Software, 
-                    con 17 años de experiencia en el sector productivo y educativo.
-                    </S.DescriptionText>
-                    <S.ContainerIcons>
-                    {socialNetworks.map((network, index)=>(
-                        <a
-                            key={index}
-                            href={network.link}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img src={network.logo} alt={network.name}/>
-                        </a>
-                    ))}
-                </S.ContainerIcons>
-                </S.ContainerText>
-                <S.ContainerImage>
-                    <img src="/img/Logo.png" alt="Iván Devia - Backend Developer"/>
-                </S.ContainerImage>
-            </S.ContainerFlex>
-        </S.ContainerAboutMe>
-    );
+  return (
+    <S.ContainerAboutMe id="about-me" style={{ marginTop: 85 }}>
+      <S.ContainerFlex>
+        <S.ContainerText>
+          <S.Title>
+            <p>👋🏼 Hello, i'm</p>
+            <p>Johan Navarro</p>
+            <p>Installation and maintenance technician</p>
+          </S.Title>
+          <S.DescriptionText>
+            <p>
+              ¡Hola! Soy técnico de sistemas apasionado por el mundo de
+              las Telecomunicaciones y con más de 8 años de experiencia en el campo.
+              Soy una persona proactiva y entusiasta, siempre buscando aprender cosas
+              nuevas y enfrentar nuevos desafíos en mi carrera profesional.
+            </p>
+            <p>
+              ¡Gracias por visitar mi portfolio! Si deseas conocer más sobre mis
+              proyectos y experiencias, no dudes en contactarme.
+            </p>
+          </S.DescriptionText>
+          <S.ContainerIcons>
+            {socialNetworks.map((network, index) => (
+              <a
+                key={index}
+                href={network.link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={network.logo} alt={network.name} />
+              </a>
+            ))}
+          </S.ContainerIcons>
+        </S.ContainerText>
+        <S.ContainerImage>
+          <img src={foto} alt="Johan Navarro R." />
+        </S.ContainerImage>
+      </S.ContainerFlex>
+    </S.ContainerAboutMe>
+  );
 };
 export default AboutMe;
